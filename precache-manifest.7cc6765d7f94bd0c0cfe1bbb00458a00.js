1b495496cd0699ca186082883ea4232eeac35c49
elenaras <elenaras@yahoo.co.uk>
2019-09-25 20:16:49 +0100

    Updates

---
+++ precache-manifest.7cc6765d7f94bd0c0cfe1bbb00458a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937e287ed4f3e714b2697913d83e61dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41c65b486729d93e42d9",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "45523eea4dae08508b2d",
+    "url": "/static/css/main.4be7abae.chunk.css"
+  },
+  {
+    "revision": "41c65b486729d93e42d9",
+    "url": "/static/js/2.75a40b51.chunk.js"
+  },
+  {
+    "revision": "45523eea4dae08508b2d",
+    "url": "/static/js/main.ca3e3431.chunk.js"
+  },
+  {
+    "revision": "4657dde2d4bac33434f3",
+    "url": "/static/js/runtime~main.6405450e.js"
+  }
+]);
